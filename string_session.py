b74053e35accf08dd9f58f5542bb6b4c7a854105
Mafia-op <96426282+Mafia-op@users.noreply.github.com>
2021-12-21 19:29:57 +0530

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -1,16 +1,8 @@
 print(
-    "ㅤ\nㅤ\n\n\n\nㅤ\n 
-
-╭━━━┳╮╱╱╭┳━━━╮╭╮╱╭╮╭━━╮╭━━━┳━━━━╮
-┃╭━━┫╰╮╭╯┃╭━╮┃┃┃╱┃┃┃╭╮┃┃╭━╮┃╭╮╭╮┃
-┃╰━━╋╮╰╯╭┫╰━━╮┃┃╱┃┃┃╰╯╰┫┃╱┃┣╯┃┃╰╯
-┃╭━━╯╰╮╭╯╰━━╮┃┃┃╱┃┃┃╭━╮┃┃╱┃┃╱┃┃
-┃╰━━┳╮┃┃╭┫╰━╯┣┫╰━╯┣┫╰━╯┃╰━╯┃╱┃┃
-╰━━━┻╯╰╯╰┻━━━┻┻━━━┻┻━━━┻━━━╯╱╰╯
-	"
+    "ㅤ\nㅤ\n\n\n\nㅤ\n┈┈┏━╮╭━┓┈╭━━━━━━╮\n┈┈┃┏┗┛┓┃╭┫EAGLEYUVI SPAMUSERBOT ┃\n┈┈╰┓▋▋┏╯╯╰━━━━━━╯\n┈╭━┻╮╲┗━━━━╮╭╮┈\n┈┃▎▎┃╲╲╲╲╲╲┣━╯┈\n┈╰━┳┻▅╯╲╲╲╲┃┈┈┈\n┈┈┈╰━┳┓┏┳┓┏╯┈┈┈\n┈┈┈┈┈┗┻┛┗┻┛┈┈┈┈┏━╮╭━┓┈╭━━━━━━╮\n┈┈┃┏┗┛┓┃╭┫EAGLEYUVI SPAMUSERBOT ┃\n┈┈╰┓▋▋┏╯╯╰━━━━━━╯\n┈╭━┻╮╲┗━━━━╮╭╮┈\n┈┃▎▎┃╲╲╲╲╲╲┣━╯┈\n┈╰━┳┻▅╯╲╲╲╲┃┈┈┈\n┈┈┈╰━┳┓┏┳┓┏╯┈┈┈\n┈┈┈┈┈┗┻┛┗┻┛┈┈┈┈┏━╮╭━┓┈╭━━━━━━╮\n┈┈┃┏┗┛┓┃╭┫EAGLEYUVI SPAMUSERBOT ┃\n┈┈╰┓▋▋┏╯╯╰━━━━━━╯\n┈╭━┻╮╲┗━━━━╮╭╮┈\n┈┃▎▎┃╲╲╲╲╲╲┣━╯┈\n┈╰━┳┻▅╯╲╲╲╲┃┈┈┈\n┈┈┈╰━┳┓┏┳┓┏╯┈┈┈\n┈┈┈┈┈┗┻┛┗┻┛┈┈┈┈┏━╮╭━┓┈╭━━━━━━╮\n┈┈┃┏┗┛┓┃╭┫EAGLEYUVI SPAMUSERBOT ┃\n┈┈╰┓▋▋┏╯╯╰━━━━━━╯\n┈╭━┻╮╲┗━━━━╮╭╮┈\n┈┃▎▎┃╲╲╲╲╲╲┣━╯┈\n┈╰━┳┻▅╯╲╲╲╲┃┈┈┈\n┈┈┈╰━┳┓┏┳┓┏╯┈┈┈\n┈┈┈┈┈┗┻┛┗┻┛┈┈"
 )
-print("\n\n\n\n\nHello sir!!, I'm Here To Help u to Generate Telethon String Session")
-print("\n\nEAGLEYUVI SPAMUSERBOT")
+print("\n\n\n\n\nABE GANDU SUNN TU STRING SESSION GENERATE KARNA KYA TU IDHAR UDHAR LADKI KO DEKH RAHA VO SETTING HAI HAMARI. ABE CHAL NA CREATE KAR STRING SESSION")
+print("\n\nEAGLEYUVI SPAMUSERBOT ")
 
 print("\n\nProperly Fill APP_ID ,HASH and Number.\n")
 
@@ -22,12 +14,12 @@ with TelegramClient(StringSession(), APP_ID, API_HASH) as hehe:
 	dcsession = hehe.session.save()
 	dEAGLE = hehe.send_message(
 	    "me",
-	    f"`{dcsession}`\n\n**Your EAGLEYUVI SPAMUSERBOT String Session Here Sir😁😎😎\nClick on above Code to Copy it\n\nFor Support Join** @BLACK_MAFIA_OP_BOLTE"
+	    f"`{dcsession}`\n\n**Your EAGLEYUVI String Session Here Sir😁😎😎\nClick on above Code to Copy it\n\nFor Support Join** @BLACK_MAFIA_OP_BOLTE"
 	)
 
 print("\n\n############################\n")
 print(
-    "check your Telegram Saved Messages For Ur New String String Session 😂😂 or see below. ")
+    "IDHAR KUCH NHI H BE GANDU SUNN TU APNE ACCOUNT KA SAVE MESSAGES OPEN KAR OR UDHAR JAA KAR DEKHLE STRING SESSION PADA HOGA 😂😂. ")
 
 print("\n############################\n")
 
