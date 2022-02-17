7905881dacf1dc4fb2998c1605338dae4ece39eb
Mafia-op <96426282+Mafia-op@users.noreply.github.com>
2022-02-17 18:16:21 +0530

    Update mafia.py

--- mafia.py
+++ mafia.py
@@ -6,7 +6,7 @@ from os import execl
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.tl.functions.account import UpdateProfileRequest
-from Config import API_ID, API_HASH, STRING, STRING2, STRING3, STRING4 ,STRING5, STRING6, STRING7, STRING8 ,STRING9, STRING10
+from Config import API_ID, API_HASH, STRING, STRING2, STRING3, STRING4 ,STRING5, STRING6, STRING7, STRING8 ,STRING9, STRING10, SUDO, BIO_MESSAGE
 import asyncio
 import telethon.utils
 from telethon.tl import functions
